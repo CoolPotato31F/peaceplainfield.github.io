ee76620919e9e45c1b89bcc675b9e70314c5f94f
Kaiser Fechner <kaiser@Kaisers-MacBook-Pro.local>
2026-02-22 09:55:19 -0600

    Auto update: 2026-02-22 09:55:19

--- load.sh
+++ load.sh
@@ -1,3 +1,5 @@
+
+# To run ./load.sh
 cd "$(dirname "$0")"
 git checkout main
 git pull origin main
--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
+# To run type ./push.sh
 git add .
 git commit -m "Auto update: $(date '+%Y-%m-%d %H:%M:%S')"
 git push
